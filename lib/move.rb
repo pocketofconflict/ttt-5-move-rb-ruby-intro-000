e866c5e15a7fee8805259c131dee16938cc13941
Christopher Diaz <pocketofconflict@gmail.com>
2021-04-23 20:16:23 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(1, 2, 3, 4, 5, 6, 7, 8, 9)
+def input_to_index{1, 2, 3, 4, 5, 6, 7, 8, 9}
   '1'.to_i
   '2'.to_i
   '3'.to_i
