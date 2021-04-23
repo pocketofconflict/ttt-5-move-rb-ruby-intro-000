72014a333b781e4d62ed9acfa91a91e6ea6dbad5
Christopher Diaz <pocketofconflict@gmail.com>
2021-04-23 20:05:26 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,13 +8,13 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index
-  puts '1'.to_i
-  puts '2'.to_i
-  puts '3'.to_i
-  puts '4'.to_i
-  puts '5'.to_i
-  puts '6'.to_i
-  puts '7'.to_i
-  puts '8'.to_i
-  puts '9'.to_i
+  puts '1'.to_i = [0]
+  puts '2'.to_i = [1]
+  puts '3'.to_i = [2]
+  puts '4'.to_i = [3]
+  puts '5'.to_i = [4]
+  puts '6'.to_i = [5]
+  puts '7'.to_i = [6]
+  puts '8'.to_i = [7]
+  puts '9'.to_i = [8]
 end
